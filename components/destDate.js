2014ec2663badc7eda54317eb5863f37957c1968
Ryan Lee <ralee478.school@gmail.com>
2021-04-21 16:12:32 -0700

    basic component created, still need to clean up style

--- components/destDate.js
+++ components/destDate.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Pressable, Image } from 'react-native';
+import { styles } from './styles'
 
-export default function myComponent() {
+export default function DestinationDate() {
     return (
-        <View>
-            {/* code goes here*/}
+        <View style={styles.destDate}>
+            <Pressable>
+                <Image source={require("./Assets/next_icon.png")}/>
+            </Pressable>
+            <Text>Timberline Lodge</Text>
+            <Text>Jan 28 - Feb 4</Text>
         </View>
     );
 }
--- components/styles.js
+++ components/styles.js
@@ -7,4 +7,22 @@ export const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
+
+    destDate: {
+      flex: 0.05,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'white',
+      width: 300,
+      borderRadius: 10,
+      shadowColor: '#000000',
+      shadowOffset: {
+        height: 0,
+        width: 0
+      },
+      shadowOpacity: 0.1,
+      shadowRadius: 1.5,
+    }
   });
