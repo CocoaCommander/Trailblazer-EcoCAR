2aeb0b6acc0f5bd00549902508d2ad90290f339e
varun99 <chawla.varun@gmail.com>
2021-04-27 12:36:58 -0700

    results

--- components/styles.js
+++ components/styles.js
@@ -7,4 +7,35 @@ export const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
+    destDate: {
+      flex: 0.05,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'white',
+      width: 300,
+      borderRadius: 10,
+      shadowColor: '#000000',
+      shadowOffset: {
+        height: 0,
+        width: 0
+      },
+      shadowOpacity: 0.1,
+      shadowRadius: 1.5,
+    },
+
+    destDateChild: {
+      flex: 1,
+      padding: 1,
+      alignItems: 'center'
+    },
+    textInput: {
+      padding: 10,
+      backgroundColor: '#eee',
+      marginVertical: 5,
+
+    }
   });
+
+  export default styles;
